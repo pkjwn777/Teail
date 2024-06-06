79641a02d51f152cb291094a638e67f945f58e6c
DESKTOP-2PFER75\kimbo <aksghk2389@naver.com>
2024-06-06 15:58:10 +0900

    user 파일 완료(controller 제외)

--- simple/src/main/java/com/simple/photo/user/UserService.java
+++ simple/src/main/java/com/simple/photo/user/UserService.java
@@ -1,6 +1,11 @@
 package com.simple.photo.user;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.simple.photo.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,5 +23,29 @@ public class UserService {
 		user.setUser_answer(user_answer);
 		this.userRepository.save(user);
 		return user;
+	}
+	
+	public List<User> getList(){
+		return this.userRepository.findAll();
+	}
+	
+	public User getUser(Long user_num) {
+		Optional<User> user = this.userRepository.findById(user_num);
+		if (user.isPresent())
+			return user.get();
+		else
+			throw new DataNotFoundException("해당 아이디가 없습니다.");
+	}
+	
+	public void modify(User user, String user_password, String user_question, String user_answer) {
+		user.setUser_password(user_password);
+		user.setUser_question(user_question);
+		user.setUser_answer(user_answer);
+		this.userRepository.save(user);
+	}
+	
+	public void delete(User user) {
+		this.userRepository.delete(user);
+		
 	}
 }
--- simple/src/main/java/com/simple/photo/userJson/UserJson.java
+++ simple/src/main/java/com/simple/photo/userJson/UserJson.java
@@ -1,5 +1,17 @@
 package com.simple.photo.userJson;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class UserJson {
 
+	@Table(name)
 }
