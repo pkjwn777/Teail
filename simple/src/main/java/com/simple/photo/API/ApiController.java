249e6af9d4df5b358c8de024b2b7bb69b917068a
pkjwn777 <142958740+pkjwn777@users.noreply.github.com>
2024-06-06 20:25:31 +0900

    Update ApiController.java
    api요청 형식 지정

--- simple/src/main/java/com/simple/photo/API/ApiController.java
+++ simple/src/main/java/com/simple/photo/API/ApiController.java
@@ -1,5 +1,25 @@
 package com.simple.photo.API;
 
-public class ApiController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import lombok.RequiredArgsConstructor;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/gemini")
+
+public class ApiController {
+  private final apiService apiService;
+
+    @GetMapping("/chat")
+    public ResponseEntity<?> gemini() {
+        try {
+            return ResponseEntity.ok().body(apiService.getContents("안녕! 너는 누구야?"));
+        } catch (HttpClientErrorException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
