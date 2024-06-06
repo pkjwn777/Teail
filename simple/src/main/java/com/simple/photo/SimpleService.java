13572805897d414df3f1aec3696c898f1612b65a
pkjwn777 <142958740+pkjwn777@users.noreply.github.com>
2024-06-06 20:08:00 +0900

    Update SimpleService.java
    GeminiService : 요청url 구성 및 RestTemplate 이용하여 api로 요청 보내기

--- simple/src/main/java/com/simple/photo/SimpleService.java
+++ simple/src/main/java/com/simple/photo/SimpleService.java
@@ -1,5 +1,40 @@
 package com.simple.photo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import com.google.api.client.util.Value;
+import lombok.RequiredArgsConstructor;
+
 public class SimpleService {
 
+}
+
+@Service
+@RequiredArgsConstructor
+public class GeminiService {
+
+    @Qualifier("geminiRestTemplate")
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${gemini.api.url}")
+    private String apiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public String getContents(String prompt) {
+
+        // Gemini에 요청 전송
+        String requestUrl = apiUrl + "?key=" + geminiApiKey;
+
+        GeminiRequest request = new GeminiRequest(prompt);
+        GeminiResponse response = restTemplate.postForObject(requestUrl, request, GeminiResponse.class);
+
+        String message = response.getCandidates().get(0).getContent().getParts().get(0).getText().toString();
+
+        return message;
+    }
 }
